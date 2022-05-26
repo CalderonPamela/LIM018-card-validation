273fb2af88b9c9eedfcd6f11a9e6c4cad77fc0cf
Pamela Calderon <pamela.calderon.canchanya@gmail.com>
2022-05-26 09:56:57 -0500

    cambio solo validacion false

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+//import { TestWatcher } from 'jest';
 import validator from './validator.js';
 
-console.log(validator);
+
+let creditCardNumber=document.getElementById('creditCardNumber');
+let ccValidator=document.getElementById('ccValidator');
+let enviarBtn=document.getElementById('button')
+//console.log(enviarBtn)
+enviarBtn.addEventListener('click', ()=>{
+
+  let message=document.getElementById("message");
+  if (creditCardNumber.value===""){
+    message="Ingrese numero de tarjeta";
+  } else {
+
+    if (validator.isValid(creditCardNumber.value)===true){
+      message="Tarjeta de credito válida";
+      } else {
+      message="Tarjeta de credito no válida";
+      ccValidator.textContent=message;
+      }
+    }
+    
+  });
+
+
+console.log(validator.isValid(creditCardNumber.value));
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,56 @@
-const validator = {
+const validator = { 
   // ...
+  isValid: function(ccNumber){
+    const length = ccNumber.length;
+    let count = 0;
+  
+    for(let i = 0; i < length; i++){
+      let currentDigit = parseInt(ccNumber[i]);
+  
+      if ((i+2) % 2 == 0)
+        if((currentDigit *=2) > 9)
+         currentDigit -=9;
+          count += currentDigit;
+    }
+    return(count % 10) === 0;
+  }
+
+
+    /*("maskify", function(ccNumber){
+    if (ccNumber.length > 4) {
+      return ccNumber;
+    } else {
+      let last4 = '';
+      for (let i = ccNumber.length - 4; i < ccNumber.length; i++) {
+        last4 += ccNumber[i];
+      }
+      let maskify = '';
+      for (let j = 0; j < ccNumber.length - 4; j++) {
+        maskify += '#';
+      }
+      return maskify + last4;
+    }
+  })*/
 };
 
-export default validator;
+/*
+    maskify: function(creditCardNumber) {
+    let starsString = creditCardNumber.slice(0, 4);
+    let showNumbers = creditCardNumber.slice(-4);
+    let regMe = /[0-9]/g;
+    let allStars = starsString.replace(regMe, '*');
+    console.log(allStars + showNumbers);
+  },
+     get maskify() {
+       return this._maskify;
+     },
+     set maskify(value) {
+       this._maskify = value;
+     },*/
+
+
+  export default validator;
+  
+
+  
+
